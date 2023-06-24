7e110788c947b8791bc96656894adef9f06e5abd
mamohammadi <mamin.mohammadi72@gmail.com>
2023-06-25 01:08:44 +0330

    Added servicce collection extension to add command handlers to DI

---
+++ src/Services/Event/Event.Common/Commands/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Event.Common.Abstractions.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Event.Common.Commands
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCommandHandlers(this IServiceCollection services)
+        {
+            var assemblyToSearch = Assembly.GetCallingAssembly();
+
+            services.AddSingleton<ICommandDispatcher, InMemoryCommandDispatcher>();
+
+            return services.Scan(s => s.FromAssemblies(assemblyToSearch)
+                           .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+                           .AsImplementedInterfaces()
+                           .WithScopedLifetime());
+        }
+    }
+}
